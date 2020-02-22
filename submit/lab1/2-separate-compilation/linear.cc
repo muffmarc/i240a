e095aca90692354f97ba64cb6524db4c30470caa
Marcus Muffoletto <mmuffol1@cs.binghamton.edu>
2020-02-22 18:07:27 -0500

    uploading lab1 code sorry raushan
    I finished the lab on the due date, as shown on the log, just somehow didn't upload the code

---
+++ submit/lab1/2-separate-compilation/linear.cc
@@ -0,0 +1,12 @@
+#include "fns-compare.hh"
+
+static double
+linear(double n)
+{
+  enum { C = 10000 };
+  return C*n;
+}
+
+
+static const bool isRegistered =
+  register_fn(FnInfo("linear", linear));
